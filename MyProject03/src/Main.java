98da75b1f845173b19ecac5c37c7a2b933dfeb42
Cinwell <cinwell@icloud.com>
2023-01-19 22:49:15 +0800

    add MyProject03

---
+++ MyProject03/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Cinwell Lau
+ * @Date ${YEAR} ${MONTH} ${DAY} ${HOUR} ${MINUTE}
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ MyProject03/src/com/cinwell/domain/Equipment.java
@@ -0,0 +1,8 @@
+package com.cinwell.domain;
+
+/**
+ * @Cinwell Lau
+ * @Date 2023 01 15 17 12
+ **/
+public interface Equipment {
+}
